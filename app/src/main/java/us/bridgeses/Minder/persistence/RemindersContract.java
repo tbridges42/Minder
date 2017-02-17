bc3fb0f93deeff7abd52e067166f254d1623cf82
Tony <tbridges42@gmail.com>
2017-02-17 08:38:34 -0600

    Add more MIME types

--- app/src/main/java/us/bridgeses/Minder/persistence/RemindersContract.java
+++ app/src/main/java/us/bridgeses/Minder/persistence/RemindersContract.java
@@ -21,7 +21,10 @@ public class RemindersContract {
 
         public static final Uri REMINDER_URI = Uri.withAppendedPath(BASE_URI, TABLE_NAME);
 
-        public static final String TYPE = "vnd.android.cursor.item/vnd.us.bridgeses.Minder.Reminder";
+        public static final String SINGLE_TYPE =
+                "vnd.android.cursor.item/vnd.us.bridgeses.Minder.Reminder";
+        public static final String MULTI_TYPE =
+                "vnd.android.cursor.dir/vnd.us.bridgeses.Minder.Reminder";
 
         public static final String COLUMN_ID = "_id";
         public static final String COLUMN_ACTIVE = "Active";
